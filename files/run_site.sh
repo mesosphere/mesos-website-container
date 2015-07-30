e71d14bd934f3c04d7fb93897b37580737ef08b8
Adam B <adam@mesosphere.io>
2015-07-29 23:41:38 -0700

    Add option to use local svn source; Run `rake` before `rake dev`.

--- files/run_site.sh
+++ files/run_site.sh
@@ -4,8 +4,14 @@ export LANG="en_US.UTF-7"
 export LANGUAGE="en_US.UTF-8"
 export LC_ALL="en_US.UTF-8"
 
-cd /tmp
-svn co https://svn.apache.org/repos/asf/mesos/site/
-cd site
-bundle install
-rake dev
+if [ -z $1 ]; then
+  echo "Pulling website source from svn"
+  cd /tmp
+  svn co https://svn.apache.org/repos/asf/mesos/site/
+  cd site
+else
+  echo "Using local website source: $1"
+  cd $1
+fi
+
+bundle install && rake && rake dev
